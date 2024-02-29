698c00a21b3b688840c63f39d37ba8609eb2ea43
Jonathan Byrne <jon.byrne27@btinternet.com>
2024-02-29 16:45:05 +0000

    Json File needs understanding

--- Year4Project/Assets/Scripts/CanvasSelection.cs
+++ Year4Project/Assets/Scripts/CanvasSelection.cs
@@ -6,12 +6,49 @@ public class CanvasSelection : MonoBehaviour
 {
     private GameManager man;
     [System.Serializable]
+    public class Meta //contains all relevant information regarding retrieval of json file
+    {
+        public string analyzer_version;
+        public string platform;
+        public string detailed_status;
+        public int status_code;
+        public int timestamp;
+        public double analysis_time;
+        public string input_process;
+    }
+    public class Track // https://stackoverflow.com/questions/45215320/how-to-take-a-particular-field-from-json-data-in-unity
+    {
+        public int num_samples;
+        public double duration;
+        public string sample_md5;
+        public int offset_seconds;
+        public int window_seconds;
+        public int analysis_sample_rate;
+        public int analysis_channels;
+        public double end_of_fade_in;
+        public double starta_of_fade_out;
+        public float loudness;
+        public float tempo;
+        public double tempo_confidence;
+        public int time_signature;
+        public double time_signature_confidence;
+        public int key;
+        public double key_confidence;
+        public int mode;
+        public double mode_confidence;
+        public string codestring;
+        public double code_version;
+        public string echoprintstring;
+        public double echoprint_version;
+        public string synchstring;
+        public double synch_version;
+        public string rhythmstring;
+        public double rhythm_version;
+    }
     public class MusicData
     {
-        public float tempo;
-        public float loudness;
-        public int key;
-        public int time_signature;
+        public List<Meta> m;
+        public List<Track> t;
     }
     public MusicData m = new MusicData();
     // Start is called before the first frame update
@@ -27,12 +64,12 @@ public class CanvasSelection : MonoBehaviour
     }
     public void MusicLoader(TextAsset pathName)
     {
-        JsonUtility.FromJson<MusicData>(pathName.text);
+        m = JsonUtility.FromJson<MusicData>(pathName.text);
         Debug.Log(pathName.text);
-        man.loudness = m.loudness;
-        man.songBpm = m.tempo;
-        man.songKey = m.key;
-        man.time_signature = m.time_signature;
+        man.loudness = m.t[0].loudness;
+        man.songBpm = m.t[0].tempo;
+        man.songKey = m.t[0].key;
+        man.time_signature = m.t[0].time_signature;
         SceneManager.LoadScene("GameScene");
     }
     public void AudioLoader(AudioClip a)
