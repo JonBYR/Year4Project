c6ea0b76274bee51feb1f592825ae7049d48f550
Jonathan Byrne <jon.byrne27@btinternet.com>
2024-05-08 14:43:37 +0100

    Revert back

--- Year4Project/Assets/Scripts/EnemySpawner.cs
+++ Year4Project/Assets/Scripts/EnemySpawner.cs
@@ -18,7 +18,7 @@ public class EnemySpawner : MonoBehaviour
     {
         man = GameManager.Instance;
         Debug.Log("The loudness is" + man.loudness);
-        if (man.loudness <= -7.1318) enemySize = 3; //loudness is measured in the API as being between -60 and 0 with -60 being the lowest
+        if (man.loudness >= -7.1318) enemySize = 3; //loudness is measured in the API as being between -60 and 0 with -60 being the lowest
         else enemySize = 5;
         enemiesSpawned = 0;
         StartCoroutine(SpawnEnemy());
